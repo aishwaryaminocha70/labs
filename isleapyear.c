e18ce88bcb9262bb1c8c6c7922acf17aa050dd44
aishwaryaminocha70 <98657660+aishwaryaminocha70@users.noreply.github.com>
2022-09-05 22:48:58 +0530

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -1,13 +1,28 @@
-ef is_leap(year):
-    leap = False
-    if year % 400 == 0:
-        leap = True
-    elif year % 100 == 0:
-        leap = False
-    elif year % 4 == 0:
-        leap = True
-    
-    return leap
-    
-year = int(raw_input())
-print is_leap(year)
+int main() {
+
+  int year;
+  cout << "Enter a year: ";
+  cin >> year;
+
+  // leap year if perfectly divisible by 400
+  if (year % 400 == 0) {
+    cout << year << " is a leap year.";
+  }
+  // not a leap year if divisible by 100
+  // but not divisible by 400
+  else if (year % 100 == 0) {
+    cout << year << " is not a leap year.";
+  }
+  // leap year if not divisible by 100
+  // but divisible by 4
+  else if (year % 4 == 0) {
+    cout << year << " is a leap year.";
+  }
+  // all other years are not leap years
+  else {
+    cout << year << " is not a leap year.";
+  }
+
+  return 0;
+}
+
