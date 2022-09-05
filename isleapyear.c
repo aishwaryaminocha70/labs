749bd80cfd3dd0d4231920db886ad19a2971df99
aishwaryaminocha70 <98657660+aishwaryaminocha70@users.noreply.github.com>
2022-09-05 22:57:12 +0530

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -1,31 +1,21 @@
-#include <iostream>
+include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
+    int year;
 
-  int year;
-  cout << "Enter a year: ";
-  cin >> year;
+    year=2000;
 
-  // leap year if perfectly divisible by 400
-  if (year % 400 == 0) {
-    cout << year << " is a leap year.";
-  }
-  // not a leap year if divisible by 100
-  // but not divisible by 400
-  else if (year % 100 == 0) {
-    cout << year << " is not a leap year.";
-  }
-  // leap year if not divisible by 100
-  // but divisible by 4
-  else if (year % 4 == 0) {
-    cout << year << " is a leap year.";
-  }
-  // all other years are not leap years
-  else {
-    cout << year << " is not a leap year.";
-  }
-
-  return 0;
+    if(year % 400 == 0)
+        cout << year << " is a Leap Year";
+        
+    else if(year % 4 == 0  && year % 100 != 0)
+        cout << year << " is a Leap Year";
+        
+    else
+        cout << year << " is not a Leap Year";
+    
+    return 0;
 }
 
