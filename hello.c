3b11e656dc6b2d5a962286aa9e667c589063f810
aishwaryaminocha70 <98657660+aishwaryaminocha70@users.noreply.github.com>
2022-09-05 22:14:08 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,8 @@
-include<iostream>
-int main() {
-   std::cout << "Hello World\n";
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World";
+    cout<<endl;
+    return 0;
 }
