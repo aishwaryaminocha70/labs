4d7e78ca6042ec0a457b96b4834009482e23bc32
aishwaryaminocha70 <98657660+aishwaryaminocha70@users.noreply.github.com>
2022-09-05 22:38:50 +0530

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -1,10 +1,18 @@
 def is_leap(year):
     leap = False
-    if year%4 == 0:
-        leap = True
-    if year%4 == 0 and year%100 == 0:
-        leap = False
-    if year%4 == 0 and year%100 == 0 and year%400 == 0:
-        leap True
-
+    # this line asks if it is divisible by 400, 4, and 100 at the same time
+    if year%4==0 and (year%100==0) and year%400==0:
+        leap=True
+    # this line asks if its not divisible by 400, 4 and 100 at the same time
+    elif not(year%4==0) and not(year%100==0) and not(year%400==0):
+        leap=False
+    
+    # you never check the case when it is divisible by 4 and not 
+    # divisible by 100, and 400.  Also skipped is if it is divisible by 
+    # 4 and 100, but not 400
+    else:
+        leap=False
+        
     return leap
+
+year = int(input())
