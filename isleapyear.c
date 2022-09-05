d15b4ef353c463362fa9b34e5b66bf4553ae6abc
aishwaryaminocha70 <98657660+aishwaryaminocha70@users.noreply.github.com>
2022-09-05 22:35:27 +0530

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -1,12 +1,16 @@
 def is_leap(year):
     leap = False
-    if year%4==0 and (year%100==0) and year%400==0:
-        leap=True
-    elif not(year%4==0) and not(year%100==0) and not(year%400==0):
-        leap=False
-    else:
-        leap=False
-        
-    return leap
 
-year = int(input())
+    # The year can be evenly divided by 4, is a leap year
+    if year%4 == 0:
+        leap = True
+
+    # Unless the year can be evenly divided by 100
+    if year%4 == 0 and year%100 == 0:
+        leap = False
+
+    # Unless the year is also evenly divisible by 400
+    if year%4 == 0 and year%100 == 0 and year%400 == 0:
+        leap True
+
+    return leap
