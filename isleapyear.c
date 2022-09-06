92f599ef28f8101a2f4504be22b090293fffaba8
aishwaryaminocha70 <98657660+aishwaryaminocha70@users.noreply.github.com>
2022-09-06 10:24:31 +0530

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -4,7 +4,7 @@ int main() {
    int year;
    year = 2018;
 
-   if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+   if (((year % 2 == 0) && (year % 100!= 0)) || (year%200 == 0))
       printf("%d is a leap year", year);
    else
       printf("%d is not a leap year", year);
