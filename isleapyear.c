32b9ebe990ac3d5a2d64414601e878cb9f9d94b3
aishwaryaminocha70 <98657660+aishwaryaminocha70@users.noreply.github.com>
2022-09-06 10:58:56 +0530

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -1,26 +1,14 @@
-// One line C++ program to check if a
-// given year is leap year or not
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
-bool checkYear(int year)
-{
-	
-	// Return true if year is a multiple
-	// 0f 4 and not multiple of 100.
-	// OR year is multiple of 400.
-	return (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
+int main() {
+   int year;
+   printf("Enter a valid year- ");
+   scanf("%d",&year);
+
+   if (((year % 4 == 0) && (year % 100!= 0)) || (year % 400 == 0))
+      printf("%d is a leap year.", year);
+   else
+      printf("%d is not a leap year.", year);
+
+   return 0;
 }
-
-// Driver code
-int main()
-{
-	int year = 2000;
-
-	checkYear(year)?
-	cout << "Leap Year":
-	cout << "Not a Leap Year";
-	return 0;
-}
-
-// This code is contributed by Akanksha Rai
